c82ef8621e67c26731b5034565691a10178a8f64
Karsten Lloyd <45316623+karstenabc@users.noreply.github.com>
2019-09-25 00:55:02 +0100

    Initial upload

---
+++ head.php
@@ -0,0 +1,20 @@
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, shrink-to-fit=no">
+<meta name="description" content="Online portfolio" />
+<link rel="icon" href="<?php echo $rootPath; ?>media/favicon.svg">
+
+<script src="<?php echo $rootPath; ?>js/jquery.js"></script>
+<script src="<?php echo $rootPath; ?>js/popper.js"></script>
+<script src="<?php echo $rootPath; ?>js/bootstrap.js"></script>
+
+<!-- Google fonts and icons -->
+<!-- <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> -->
+<!-- <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> -->
+<!-- <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:light,regular,medium,thin,italic,mediumitalic,bold" title="roboto"> -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,400italic,700">
+
+<!-- CSS -->
+<link rel="stylesheet" href="<?php echo $rootPath; ?>css/bootstrap.min.css" />
+<link rel="stylesheet" href="<?php echo $rootPath; ?>css/navbar.css" type="text/css" />
+
+<link rel="stylesheet" href="<?php echo $rootPath; ?>css/stylesheet.css" />
