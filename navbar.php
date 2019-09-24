3833db30bda40ddf2754635239d6c07418faf772
Karsten Lloyd <45316623+karstenabc@users.noreply.github.com>
2019-09-25 00:57:21 +0100

    Created navbar and added new logo to it.

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<!-- Image and text -->
+<nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="index.php">
+        <img src="media/logo3/vector/isolated-monochrome-white.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+        Karsten Lloyd
+    </a>
+
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link active" href="portfolio.php">portfolio</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">contact</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" href="#">link</a>
+        </li>
+    </ul>
+</nav>
