7ddb9f4f8a806ab5c001e789fb1d5dbdd21d4e34
Karsten Lloyd <45316623+karstenabc@users.noreply.github.com>
2019-10-06 22:12:15 +0100

    Added go to top button.

--- navbar.php
+++ navbar.php
@@ -17,3 +17,22 @@
         </li>
     </ul>
 </nav>
+
+<button onclick="topFunction()" id="toTop" title="Go to top">&#8673;</button>
+
+<script>
+    window.onscroll = function() {
+        scrollFunction()
+    };
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("toTop").style.display = "block";
+        } else {
+            document.getElementById("toTop").style.display = "none";
+        }
+    }
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+</script>
