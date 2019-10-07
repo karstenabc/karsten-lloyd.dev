6852bda2305475ee8e3a39ecf4841894c7b50af3
Karsten Lloyd <45316623+karstenabc@users.noreply.github.com>
2019-10-07 22:32:03 +0100

    Fixed footer to bottom of page. Added space above footer.

--- private/footer.php
+++ private/footer.php
@@ -5,9 +5,13 @@
         background-color: #3b9bd3;
         text-align: center;
         padding: 8px;
+        position: fixed;
+        bottom: 0px;
+        left: 0px;
+        width: 100%;
     }
 </style>
-
+<br /><br />
 <div class="footer">
     Karsten Lloyd 2019
 </div>
