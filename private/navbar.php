71971b5a5245f20804eb6705d66d022408f2582e
Karsten Lloyd <45316623+karstenabc@users.noreply.github.com>
2019-10-08 13:56:41 +0100

    Added mobile friendly navbar. Fixed border issues on "Recent Projects".

--- private/navbar.php
+++ private/navbar.php
@@ -1,21 +1,27 @@
-<!-- Image and text -->
-<nav class="navbar navbar-light bg-light">
+<nav class="navbar navbar-expand-sm navbar-light">
     <a class="navbar-brand" href="index.php">
         <img src="media/logo3/vector/isolated-monochrome-white.svg" width="30" height="30" class="d-inline-block align-top" alt="">
         <?php echo ($activePage == "home") ? '' : 'Karsten Lloyd'; ?>
     </a>
 
-    <ul class="nav justify-content-center">
-        <li class="nav-item <?php echo ($activePage == "home") ? 'active' : ''; ?>">
-            <a class="nav-link" href="index">home</a>
-        </li>
-        <li class="nav-item <?php echo ($activePage == "portfolio") ? 'active' : ''; ?>">
-            <a class="nav-link" href="portfolio.php">portfolio</a>
-        </li>
-        <li class="nav-item <?php echo ($activePage == "contact") ? 'active' : ''; ?>">
-            <a class="nav-link" href="contact.php">contact</a>
-        </li>
-    </ul>
+    <!-- Toggler/collapsibe Button -->
+    <button class="navbar-toggler menu-toggle" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+      <img src="media/icons/menu.svg"/>
+    </button>
+
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="nav navbar-nav">
+            <li class="nav-item <?php echo ($activePage == "home") ? 'active' : ''; ?>">
+                <a class="nav-link" href="index">home</a>
+            </li>
+            <li class="nav-item <?php echo ($activePage == "portfolio") ? 'active' : ''; ?>">
+                <a class="nav-link" href="portfolio.php">portfolio</a>
+            </li>
+            <li class="nav-item <?php echo ($activePage == "contact") ? 'active' : ''; ?>">
+                <a class="nav-link" href="contact.php">contact</a>
+            </li>
+        </ul>
+    </div>
 </nav>
 
 <button onclick="topFunction()" id="toTop" title="Go to top">&#8673;</button>
