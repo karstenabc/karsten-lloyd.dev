27a06ea9499b0cfb0d4d93cdf3455c0ab626b369
Tim Horwood <timhorwood@Tims-MacBook-Pro.local>
2020-06-22 09:47:23 +0100

    Updated couple of things

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
+import React, { Component } from 'react'
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/font.module.css'
 import Link from "next/link";
-import React from "react";
 
-export default function Home() {
-  return (
-      <Layout page={"home"}>
-        <Head>
-          <title>test</title>
-        </Head>
-        <section className={utilStyles.headingMd}>
-          <p>[Your Self Introduction]</p>
-          <p>
-            (This is a sample website - you’ll be building a site like this on{' '}
-            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-          </p>
-          {/*<Link href="projects/advent-of-code/2018"><a>link</a></Link>*/}
-        </section>
-      </Layout>
-  )
-}
+const Home = () => (
+  <Layout page={"home"}>
+    <Head>
+      <title>test</title>
+    </Head>
+    <section className={utilStyles.headingMd}>
+      <p>[Your Self Introduction]</p>
+      <p>
+        (This is a sample website - you’ll be building a site like this on{' '}
+        <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+      </p>
+      {/*<Link href="projects/advent-of-code/2018"><a>link</a></Link>*/}
+    </section>
+  </Layout>
+);
+
+export default Home;
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Component } from "react"
 import Head from "next/head"
 import fetch from 'isomorphic-unfetch'
 import styles from '../components/layout.module.css'
@@ -7,30 +7,14 @@ import Row from "react-bootstrap/Row"
 import Layout from "../components/layout";
 import Card from "../components/card"
 import Link from "next/link";
-
-
-// Get page data from DB
-Portfolio.getInitialProps = async ({ req, query }) => {
-  let pageRequest = `https://api.karsten-lloyd.dev/site.php/experience`;
-  let res = await fetch(pageRequest);
-  let experience = await res.json();
-  pageRequest = `https://api.karsten-lloyd.dev/site.php/projects`;
-  res = await fetch(pageRequest);
-  let projects = await res.json();
-  pageRequest = `https://api.karsten-lloyd.dev/site.php/education`;
-  res = await fetch(pageRequest);
-  let education = await res.json();
-  return { experience: experience, projects: projects, education: education }
-};
-
+import Nav from "react-bootstrap/Nav";
 
 // Render rows of pills
 function renderPills(array) {
   const pills = [];
-  array.every(item => pills.push(<span className={styles.pill} key={item}>{item}</span> ));
+  array.every(item => pills.push(<span className={styles.pill} key={item}>{item}</span> )); //Can change to .map 
   return pills;
 }
-
 
 // Render view all button
 function viewAll(title, link) {
@@ -38,31 +22,45 @@ function viewAll(title, link) {
       <>
         <Row>
           <div className={styles.viewAll}>
-            <Link href="portfolio">
-              <a>View All {title}</a>
-            </Link>
-            {/*<a href={`/portfolio/`+link}>View All {title}</a>*/}
+            <Nav.Link href={`/portfolio/${link}`}>
+              View All {title}
+            </Nav.Link>
           </div>
         </Row>
       </>
   );
 }
 
+class Portfolio extends Component {
+  static async getInitialProps({ req }) {
+    let pageRequest = `https://api.karsten-lloyd.dev/site.php/experience`;
+    let res = await fetch(pageRequest);
+    let experience = await res.json();
+    pageRequest = `https://api.karsten-lloyd.dev/site.php/projects`;
+    res = await fetch(pageRequest);
+    let projects = await res.json();
+    pageRequest = `https://api.karsten-lloyd.dev/site.php/education`;
+    res = await fetch(pageRequest);
+    let education = await res.json();
+    return { experience: experience, projects: projects, education: education }
+  }
+  constructor(props) {
+      super(props);
+      this.state = {}
+  }
+  render() {
+    const { experience, projects, education } = this.props;
 
-function Portfolio({ experience, projects, education }) {
-  const title = "Portfolio | Karsten Lloyd";
-  const section = "portfolio";
-  const heading = "PORTFOLIO";
-  const subheading = <>My <a href="#languages">languages</a>, <a href="#experience">experience</a>, <a href="#projects">projects</a> and <a href="#education">education</a></>;
+    const title = "Portfolio | Karsten Lloyd";
+    const section = "portfolio";
+    const heading = "PORTFOLIO";
+    const subheading = <>My <a href="#languages">languages</a>, <a href="#experience">experience</a>, <a href="#projects">projects</a> and <a href="#education">education</a></>;
 
-  // Pills
-  const languages = ['CSS', 'Dart', 'HTML', 'JavaScript', 'PHP', 'Python', 'SQL'];
-  const frameworks = ['BootStrap', 'Flutter', 'NextJS', 'jQuery', 'React-Native', 'RedBean', 'Twig', 'xlutils'];
-  const tools = ['Android Studio', 'Git', 'phpMyAdmin'];
-
-
-  return (
-      <>
+    // Pills
+    const languages = ['CSS', 'Dart', 'HTML', 'JavaScript', 'PHP', 'Python', 'SQL'];
+    const frameworks = ['BootStrap', 'Flutter', 'NextJS', 'jQuery', 'React-Native', 'RedBean', 'Twig', 'xlutils'];
+    const tools = ['Android Studio', 'Git', 'phpMyAdmin'];
+      return (
         <Layout page={section} header={<><h1>{heading}</h1><p className={styles.subheading}>{subheading}</p></>}>
           <Head>
             <title>{title}</title>
@@ -82,8 +80,8 @@ function Portfolio({ experience, projects, education }) {
 
             <h2 id="experience">Experience</h2>
             <Row>
-              {experience.results.slice(0,3).map(e => (
-                  <div className="col-sm-12 col-md-6 col-lg-4">
+              {experience.results.slice(0,3).map((e, index) => (
+                  <div key={index} className="col-sm-12 col-md-6 col-lg-4">
                     { Card('experience', e) }
                   </div>
               ))}
@@ -93,8 +91,8 @@ function Portfolio({ experience, projects, education }) {
 
             <h2 id="projects">Projects</h2>
             <Row>
-              {projects.results.slice(0,3).map(e => (
-                  <div className="col-sm-12 col-md-6 col-lg-4">
+              {projects.results.slice(0,3).map((e, index) => (
+                  <div key={index} className="col-sm-12 col-md-6 col-lg-4">
                     { Card('project', e) }
                   </div>
               ))}
@@ -104,16 +102,16 @@ function Portfolio({ experience, projects, education }) {
 
             <h2 id="education">Education</h2>
             <Row>
-              {education.results.map(e => (
-                  <div className="col-sm-12 col-md-6 col-lg-4">
+              {education.results.map((e, index) => (
+                  <div key={index} className="col-sm-12 col-md-6 col-lg-4">
                     { Card('education', e) }
                   </div>
               ))}
             </Row>
           </Container>
         </Layout>
-      </>
-  );
+      )
+  }
 }
 
 export default Portfolio
